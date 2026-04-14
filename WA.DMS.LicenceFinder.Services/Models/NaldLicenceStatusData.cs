b992b52b9fae05b80c5e72916fc1e4d30aa01d62
Ryan Barlow <ryan.barlow@environment-agency.gov.uk>
2026-04-14 11:09:54 +0100

    Match updated API shape

--- WA.DMS.LicenceFinder.Services/Models/NaldLicenceStatusData.cs
+++ WA.DMS.LicenceFinder.Services/Models/NaldLicenceStatusData.cs
@@ -2,9 +2,15 @@ namespace WA.DMS.LicenceFinder.Services.Models;
 
 public class NaldLicenceStatusData
 {
+    // Abstraction licences
     public HashSet<string> LiveLicences { get; set; } = [];
 
-    public HashSet<string> DeadLicences { get; set; } = [];
+    public HashSet<string> RevokedLicences { get; set; } = [];
     
+    public HashSet<string> LapsedLicences { get; set; } = [];
+    
+    public HashSet<string> ExpiredLicences { get; set; } = [];
+    
+    // Impoundment licences
     public HashSet<string> ImpoundmentLicences { get; set; } = [];
 }
